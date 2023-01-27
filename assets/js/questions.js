126a4395a7b92e9c114f1ade2e6c038aefc8ab74
Yvonne Adedeji <yvonne.sarah.adedeji@hotmail.com>
2023-01-27 16:48:47 +0000

    Questions for the JavaScript quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,83 @@
+//  list of all questions, choices, and answers in a array of Objects
+var questions = [
+{
+title: "1. What is JavaScript?:",
+answer: "JavaScript is a scripting language used to make the website interactive",
+choices: [
+"JavaScript is a scripting language used to make the website interactive",
+"JavaScript is an assembly language used to make the website interactive",
+"JavaScript is a compiled language used to make the website interactive",
+"None of the mentioned"
+]
+},
+{
+title: "2. Which of the following scoping type does JavaScript use?",
+answer: "Lexical",
+choices: [
+"Sequential",
+"Segmental",
+"Lexical",
+"Literal"
+]
+},
+{
+title: "3. String values must be enclosed within ____ when being assigned to variables.",
+answer: "Quotes",
+choices: [
+"Commas",
+"Curly brackets",
+"Quotes",
+"Parenthesis"
+]
+},
+{
+title: "4. Why event handlers is needed in JS?",
+answer: "Allows JavaScript code to alter the behaviour of windows",
+choices: [
+"Allows JavaScript code to alter the behaviour of windows",
+"Adds innerHTML page to the code",
+"Change the server location",
+"Performs handling of exceptions and occurrences"
+]
+},
+{
+title: "5. In JavaScript, what is a block of statement?",
+answer: "Block that combines a number of statements into a single compound statement",
+choices: [
+"Conditional block",
+"Block that combines a number of statements into a single compound statement",
+"Both conditional block and a single statement",
+"Block that contains a single statement"
+]
+},
+{
+title: "6. The “function” and “var” are known as____",
+answer: "Declaration statements",
+choices: [
+"Keywords",
+"Data types",
+"Prototypes",
+"Declaration statements"
+]
+},
+{
+title: "7. JavaScript code can be written in ____.",
+answer: "JavaScript file and in HTML document directly",
+choices: [
+"JavaScript file (.js file)",
+"HTML document directly",
+"JavaScript file and in HTML document directly",
+"In style sheets (.css file)"
+]
+},
+{
+title: "8. What is the correct syntax for referring to an external script called xxx.js?",
+answer: "<script src=“xxx.js”>",
+choices: [
+"<script name=“xxx.js”>",
+"<script src=“xxx.js”>",
+"<script href=“xxx.js”>",
+"None of the above"
+    ]
+  },
+];
